0a08a898a693d118f57548ca760f6cc8d47eaa9a
DoneandDani <smolestlemon@gmail.com>
2026-08-21 12:33:17 +0100

    [Sessão 2] Implemententação do Player Avatar, basic movment and basic map

--- LOG.md
+++ LOG.md
@@ -18,3 +18,35 @@ A minha instalação do C está com problemas e o VScode não reconhece o compil
 Resolver o problema relacionado com o compilador e com a Ncurses;
 Criar a estrutura básica do jogo.
 **Linhas de código escritas hoje (estimativa):**
+
+
+## Sessão 2 – 21/08/2026
+**Funcionalidades implementadas hoje:**
+
+- O Ncurses está a funcionar!
+
+- Implementei a base que será usada para mover o  Player e guardar informações sobre o mesmo através da estrutura Struct; 
+    - Aprendi bastante sobre boas práticas de como implementar este tipo de objeto (se usar typedef vs só struct) [aqui] (https://stackoverflow.com/questions/1675351/typedef-struct-vs-struct-definitions);
+- Implementei uma função rudimentar que desenha o mapa (de momento apenas uma sala);
+- Implementei a função que altera o struct Player e permite dar update a posição do utilizador e que dá print ao 'Avatar' do mesmo (@); 
+- Implementei uma função que verifica o carácter onde o cursor está, que permite em conjunto com a função que move o Avatar impedir o utilizador de atravessar paredes. Permitirá no futuro ao jogo identificar se o utilizador está a mover para uma posição com tesouro,monstros, etc.
+
+
+
+**Maior dificuldade encontrada e como resolvi (ou não resolvi):**
+
+- Através de pesquisa online sobre o funcionamento do VSCode e do próprio C consegui ligar a biblioteca NCurses ao meu projeto.
+    - Contudo não consegui colocar o terminal do VSCode a correr o terminal associado ao UCRT, dado que tenho que usar o terminal do mesmo para testar o código.
+- Aprender como utilizar os pointers à struct Player e fazer as funções de mover o avatar e detetar o carácter funcionarem.
+    - Aprender mais sobre pointers em geral.
+
+-
+**Próximo passo planeado:**
+
+- Implementar uma função que crie partes do mapa (rooms), verificar a viabilidade de criar um gerador de salas aleatório;
+- Implementar inimigos rudimentares;  
+
+**Linhas de código escritas hoje (estimativa):**
+
+- 130 linhas +-;
+..
--- main.c
+++ main.c
@@ -1,3 +1,151 @@
 #include <stdio.h>
-
 #include<ncurses.h>
+#include<stdlib.h>
+
+#define esc 27 //Macro for the esc key in ASCII
+
+typedef struct Player
+{
+    int xcord;
+    int ycord;
+    int HP;
+} Player ;
+
+
+
+int screen_setup();
+int map_maker();
+Player * player_maker(); //Promising a function that return a pointere to the struct Player, I think.
+int handle_input(int input, Player * user);
+int player_move(int y, int  x, Player* user );
+int check_move( int tempY, int tempX, Player* user);
+
+
+int main(){
+    
+    Player * user; //user is declared as a pointer to the struct Player
+    int userInput;
+
+    screen_setup();
+    map_maker();
+    user = player_maker();
+    //Main game loop, ends when user pressess the esc key
+    while ((userInput =getch()) != esc){
+        handle_input(userInput, user);
+    }
+    
+    refresh();
+    
+
+    endwin();
+    return 0;
+    
+}
+
+
+int screen_setup(){
+    initscr();
+    printw("Hello world!");
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+
+    return 1;
+}
+
+int map_maker(){
+
+    mvprintw(13,13,"---------");
+    mvprintw(14,13,"|.......|");
+    mvprintw(15,13,"|.......|");
+    mvprintw(16,13,"|.......|");
+    mvprintw(17,13,"---------");
+
+    return 0;
+
+
+}
+
+Player * player_maker(){
+
+    Player *initPlayer;
+    initPlayer= malloc(sizeof(Player)); //Dynamically allocated the size in memory necessary for the newPlayer pointer based on the size of Player,
+                                       // as I don't know beforehand how big it's going to be (+ I'll constantly add changes to the Player struct itself).
+    initPlayer-> xcord =14; //Acessing the coordinates in the Player struct and changing their values
+    initPlayer-> ycord =14;
+    initPlayer->HP= 5;
+
+    player_move(14,14, initPlayer );
+    
+    return initPlayer;
+}
+
+
+int handle_input(int input, Player * user){
+    int tempY;
+    int tempX;
+
+    switch (input)
+    {
+        case KEY_UP:
+            tempY = user->ycord -1;
+            tempX = user->xcord;
+            
+            break;
+
+        case KEY_DOWN:
+            tempY = user->ycord +1;
+            tempX = user->xcord;
+            
+            break;
+        
+        case KEY_LEFT:
+            tempY = user->ycord ;
+            tempX = user->xcord -1;
+            
+            break;
+        
+        case KEY_RIGHT:
+            tempY = user->ycord;
+            tempX = user->xcord +1;
+            
+            break;
+
+        default:
+            break;
+
+    }
+    check_move(tempY,tempX,user);
+
+}
+
+int player_move(int y, int  x, Player* user ){
+
+    mvprintw(user->ycord, user->xcord, ".");
+
+    user->ycord =y;
+    user->xcord =x;
+
+    mvprintw(user->ycord, user->xcord, "@"); //Printing the Player 'avatar' on the screen
+    move(user->ycord, user->xcord);          //moves the cursor back to the place of the avatar, since by default mvprintw moves it a space foward.
+
+}
+
+// Check what is at next position. Will get updated later but for now just makes sure the Avatar can't move through walls.
+int check_move( int tempY, int tempX, Player* user){
+
+    int tempP;
+    switch (mvinch(tempY,tempX)) //Function from Ncurses that checks what character the cursor is on
+    {
+        case '.':
+            player_move(tempY,tempX, user);
+            break;
+        default:
+            move(user->ycord,user->xcord);
+            break;
+
+    }
+
+
+
+}
